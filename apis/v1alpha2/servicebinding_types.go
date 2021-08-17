3fd7947a73603893e6926af755e89e0317f3d0b4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-08-17 19:39:53 +0530

    Bootstrap project using kubebuilder 3.1.0  (#1)
    * Bootstrap project using kubebuilder 3.1.0
    
    Commands used to bootstrap:
    
    kubebuilder init --domain="service.binding" --repo="github.com/kubernetes-sigs/service-binding" --owner="The Kubernetes Authors" --project-name="service-binding-operator"
    kubebuilder edit --multigroup=true
    kubebuilder create api --crd-version="v1" --group="" --version="v1alpha2" --kind="ServiceBinding" --resource --controller
    kubebuilder create api --crd-version="v1" --group="" --version="v1alpha2" --kind ClusterApplicationResourceMapping --resource --controller
    make generate
    
    * ClusterApplicationResourceMapping is cluster scoped

---
+++ apis/v1alpha2/servicebinding_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ServiceBindingSpec defines the desired state of ServiceBinding
+type ServiceBindingSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ServiceBinding. Edit servicebinding_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ServiceBindingStatus defines the observed state of ServiceBinding
+type ServiceBindingStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ServiceBinding is the Schema for the servicebindings API
+type ServiceBinding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ServiceBindingSpec   `json:"spec,omitempty"`
+	Status ServiceBindingStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ServiceBindingList contains a list of ServiceBinding
+type ServiceBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ServiceBinding `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ServiceBinding{}, &ServiceBindingList{})
+}
