586f21a9a162fb2395beb69dfd75bc444b1d7a29
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-08-18 07:05:35 +0530

    Copy the API schema defined in the spec
    Signed-off-by: Baiju Muthukadan <baiju.m.mail@gmail.com>

--- apis/v1alpha2/clusterapplicationresourcemapping_types.go
+++ apis/v1alpha2/clusterapplicationresourcemapping_types.go
@@ -1,65 +1,62 @@
 /*
-Copyright 2021 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2021 The Kubernetes Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package v1alpha2
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// ClusterApplicationResourceMappingVersion defines the mapping for a specific version of an application resource.
+type ClusterApplicationResourceMappingVersion struct {
+	// Version is the version of the application resource that this mapping is for.
+	Version string `json:"version"`
+	// Containers is the collection of JSONPaths that container configuration may be written to.
+	Containers []string `json:"containers,omitempty"`
+	// Envs is the collection of JSONPaths that env configuration may be written to.
+	Envs []string `json:"envs,omitempty"`
+	// VolumeMounts is the collection of JSONPaths that volume mount configuration may be written to.
+	VolumeMounts []string `json:"volumeMounts,omitempty"`
+	// Volumes is the JSONPath that volume configuration must be written to.
+	Volumes string `json:"volumes"`
+}
 
 // ClusterApplicationResourceMappingSpec defines the desired state of ClusterApplicationResourceMapping
 type ClusterApplicationResourceMappingSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of ClusterApplicationResourceMapping. Edit clusterapplicationresourcemapping_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Versions is the collection of versions for a given resource, with mappings.
+	Versions []ClusterApplicationResourceMappingVersion `json:"versions,omitempty"`
 }
 
-// ClusterApplicationResourceMappingStatus defines the observed state of ClusterApplicationResourceMapping
-type ClusterApplicationResourceMappingStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-//+kubebuilder:resource:scope=Cluster
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:storageversion
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
 // ClusterApplicationResourceMapping is the Schema for the clusterapplicationresourcemappings API
 type ClusterApplicationResourceMapping struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ClusterApplicationResourceMappingSpec   `json:"spec,omitempty"`
-	Status ClusterApplicationResourceMappingStatus `json:"status,omitempty"`
+	Spec ClusterApplicationResourceMappingSpec `json:"spec,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
 // ClusterApplicationResourceMappingList contains a list of ClusterApplicationResourceMapping
 type ClusterApplicationResourceMappingList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []ClusterApplicationResourceMapping `json:"items"`
-}
 
-func init() {
-	SchemeBuilder.Register(&ClusterApplicationResourceMapping{}, &ClusterApplicationResourceMappingList{})
+	Items []ClusterApplicationResourceMapping `json:"items"`
 }
