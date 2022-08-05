2642f63a549a0d37d661c02d6392cb16194c6569
Scott Andrews <andrewssc@vmware.com>
2022-08-04 21:06:47 -0400

    review feedback
    Signed-off-by: Scott Andrews <andrewssc@vmware.com>

--- hack/ko/tools.go
+++ hack/ko/tools.go
@@ -2,7 +2,7 @@
 // +build tools
 
 /*
-Copyright 2022 the original author or authors.authors.
+Copyright 2022 the original author or authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
